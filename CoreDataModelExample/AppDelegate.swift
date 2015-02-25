4e851b070c733a9751c2a4b460a54c9be1261e2b
Clay Tinnell <ctinnell@gaig.com>
2015-02-25 11:02:34 -0500

    Added call to initialize the CoreDataManager. The framework is working well.

--- CoreDataModelExample/AppDelegate.swift
+++ CoreDataModelExample/AppDelegate.swift
@@ -13,10 +13,12 @@ import CoreDataModel
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
+    lazy var coreDataManager: CoreDataManager? = CoreDataManager()
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        var person: Person
+        Person.loadTestData(coreDataManager?.managedObjectContext)
         return true
     }
 
@@ -32,6 +34,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
@@ -41,7 +44,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
 
 }
 
